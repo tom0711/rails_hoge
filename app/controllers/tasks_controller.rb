f8bb3a117cdb2c132a3524e25f21dd4030d027d9
Takuya Tominaga <tom.eng.pg.03tw@gmail.com>
2019-07-17 00:11:53 +0900

    rails generate scaffold

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,81 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+  # GET /tasks
+  # GET /tasks.json
+  # 一覧表示画面を表示する
+  def index
+    @tasks = Task.all
+  end
+
+  # GET /tasks/1
+  # GET /tasks/1.json
+  # 詳細表示画面を表示する
+  def show
+  end
+
+  # GET /tasks/new
+  # 新規登録画面を表示する
+  def new
+    @task = Task.new
+  end
+
+  # GET /tasks/1/edit
+  # 編集画面を表示する
+  def edit
+  end
+
+  # POST /tasks
+  # POST /tasks.json
+  # 新規登録処理をして、詳細画面を表示する（POST）
+  def create
+    @task = Task.new(task_params)
+
+    respond_to do |format|
+      if @task.save
+        format.html { redirect_to @task, notice: 'Task was successfully created.' }
+        format.json { render :show, status: :created, location: @task }
+      else
+        format.html { render :new }
+        format.json { render json: @task.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /tasks/1
+  # PATCH/PUT /tasks/1.json
+  # 更新処理をして、詳細画面を表示する（PUT/PATCH）
+  def update
+    respond_to do |format|
+      if @task.update(task_params)
+        format.html { redirect_to @task, notice: 'Task was successfully updated.' }
+        format.json { render :show, status: :ok, location: @task }
+      else
+        format.html { render :edit }
+        format.json { render json: @task.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /tasks/1
+  # DELETE /tasks/1.json
+  # 削除処理をして、一覧画面を表示する
+  def destroy
+    @task.destroy
+    respond_to do |format|
+      format.html { redirect_to tasks_url, notice: 'Task was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_task
+      @task = Task.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def task_params
+      params.require(:task).permit(:content)
+    end
+end
